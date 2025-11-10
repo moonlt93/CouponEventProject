9f4df6533b1aa33e69483620df8ba19ce4d65471
jinsu <moonlt93@github.com>
2025-11-10 15:47:10 +0900

    feat: init project

---
+++ src/main/kotlin/org/example/couponeventproject/CouponEventProjectApplication.kt
@@ -0,0 +1,11 @@
+package org.example.couponeventproject
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CouponEventProjectApplication
+
+fun main(args: Array<String>) {
+    runApplication<CouponEventProjectApplication>(*args)
+}
